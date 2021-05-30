ca8468dcab9866d4593ae14fb613ea6c45a7f503
kanchitbajaj8070 <31831699+kanchitbajaj8070@users.noreply.github.com>
2021-05-31 02:38:50 +0530

    week1 first commit

---
+++ src/Recursion/week1/Fibonacci.java
@@ -0,0 +1,21 @@
+package Recursion.week1;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n=10;
+        // 0 1 1 2 3 5 8 13 21 34 55
+        System.out.println(nthFibonacciTerm(n));
+    }
+    private static int nthFibonacciTerm ( int n)
+    {
+        if( n==1)
+            return 0;//1st fibonacci term
+        if( n==2)
+            return 1;//2nd fibonacci term
+
+        int nMinusOneTerm=nthFibonacciTerm(n-1);
+        int nMinusTwoTerm=nthFibonacciTerm(n-2);
+        return nMinusOneTerm+nMinusTwoTerm;
+
+    }
+}
